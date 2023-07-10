fc6e3b14cc45958572ae3c5a9a384c4a32fe7e7b
lejlamrt <lejlamuratovic6@gmail.com>
2023-07-10 22:34:34 +0200

    added form validation and moved login logic from routes to services

--- assets/js/user-service.js
+++ assets/js/user-service.js
@@ -1,6 +1,22 @@
 var UserService = {
 	init: function () {
 		$("#login-form").validate({
+			rules: {
+				email: {
+					required: true,
+				},
+				password: {
+					required: true,
+				},
+			},
+			messages: {
+				email: {
+					required: "Email is required",
+				},
+				password: {
+					required: "Password is required",
+				},
+			},
 			submitHandler: function (form, event) {
 				event.preventDefault();
 				$("#signin-btn").prop("disabled", true);
@@ -8,8 +24,42 @@ var UserService = {
 				UserService.login(entity);
 			},
 		});
-
+	
 		$("#signup-form").validate({
+			rules: {
+				first_name: {
+					required: true,
+				},
+				last_name: {
+					required: true,
+				},
+				email: {
+					required: true,
+					email: true,
+				},
+				password: {
+					required: true,
+					minlength: 8,
+					regex: /[!@#$%^&*]/,
+				},
+			},
+			messages: {
+				first_name: {
+					required: "First name is required",
+				},
+				last_name: {
+					required: "Last name is required",
+				},
+				email: {
+					required: "Email is required",
+					email: "Please enter a valid email address",
+				},
+				password: {
+					required: "Password is required",
+					minlength: "Password must be at least 8 characters long",
+					regex: "Password must contain at least one special character",
+				},
+			},
 			submitHandler: function (form, event) {
 				event.preventDefault();
 				$("#signup-btn").prop("disabled", true);
@@ -19,6 +69,15 @@ var UserService = {
 		});
 	},
 
+	displayError: function (message, targetId) {
+		var errorHtml = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+		  <strong>Error!</strong> ${message}
+		  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+		</div>`;
+	
+		$(`#${targetId}`).html(errorHtml);
+	},
+	
 	login: function (entity) {
 		$.ajax({
 			url: "rest/users/login",
@@ -33,7 +92,7 @@ var UserService = {
 			},
 			error: function (XMLHttpRequest, textStatus, errorThrown) {
 				$("#signin-btn").prop("disabled", false);
-				alert("Wrong email or password");
+           		UserService.displayError("Wrong email or password", "login-error");
 			},
 		});
 	},
@@ -45,20 +104,29 @@ var UserService = {
 
 	signup: function (entity) {
 		$.ajax({
-			url: "rest/users/signup",
-			type: "POST",
-			data: JSON.stringify(entity),
-			contentType: "application/json",
-			dataType: "json",
-			success: function (result) {
-				console.log(result);
-				alert("User created successfully");
-				window.location.replace("login.html?action=login");
-			},
-			error: function (XMLHttpRequest, textStatus, errorThrown) {
-				$("#signup-btn").prop("disabled", false);
-				alert("Failed to create user");
-			},
+		  url: "rest/users/signup",
+		  type: "POST",
+		  data: JSON.stringify(entity),
+		  contentType: "application/json",
+		  dataType: "json",
+		  success: function (result) {
+			console.log(result);
+			alert("User created successfully");
+			window.location.replace("login.html?action=login");
+		  },
+		  error: function (XMLHttpRequest, textStatus, errorThrown) {
+			$("#signup-btn").prop("disabled", false);
+			if (XMLHttpRequest.responseJSON && XMLHttpRequest.responseJSON.message === "Email address already exists") {
+			  UserService.displayError("Email address already exists", "signup-error");
+			} else {
+			  UserService.displayError("Failed to create user", "signup-error");
+			}
+		  },
 		});
-	},
+	  },
+	  
 };
+
+$.validator.addMethod('regex', function (value, element, param) {
+	return this.optional(element) || value.match(typeof param === 'string' ? new RegExp(param) : param);
+}, 'Please enter a valid value');
