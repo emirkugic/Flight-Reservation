7ee53ca3d4fcba0de1fb6ff86ee53253a2a79a61
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-07-11 03:04:39 +0200

    flight form validation

--- assets/js/flight-form.js
+++ assets/js/flight-form.js
@@ -1,10 +1,62 @@
-$(document).ready(function () {
-	$("#one-way").click(function () {
-		$("#return-date-input").hide();
-		$("#departure-date").css("width", "400px");
-	});
-	$("#roundtrip").click(function () {
-		$("#return-date-input").show();
-		$("#departure-date").css("width", "185px");
-	});
+$(document).ready(function() {
+    // Add method for date validation
+    $.validator.addMethod("date", function(value, element) {
+        return this.optional(element) || !/Invalid|NaN/.test(new Date(value).toString());
+    }, "Please enter a valid date.");
+
+    // Validation for search flight form
+    $("#flight-search-form").validate({
+        rules: {
+            "from-input": {
+                required: true,
+            },
+            "to-input": {
+                required: true,
+            },
+            "departure-date": {
+                required: true,
+                date: true
+            },
+            "return-date": {
+                required: true,
+                date: true
+            },
+            passengers: {
+                required: true,
+            },
+            "ticket-class": {
+                required: true,
+            }
+        },
+        messages: {
+            "from-input": {
+                required: "From field is required",
+            },
+            "to-input": {
+                required: "To field is required",
+            },
+            "departure-date": {
+                required: "Departure date is required",
+            },
+            "return-date": {
+                required: "Return date is required",
+            },
+            passengers: {
+                required: "Passenger field is required",
+            },
+            "ticket-class": {
+                required: "Ticket class field is required",
+            }
+        }
+    });
+
+    $("#one-way").click(function() {
+        $("#return-date-input").hide();
+        $("#departure-date").css("width", "400px");
+    });
+
+    $("#roundtrip").click(function() {
+        $("#return-date-input").show();
+        $("#departure-date").css("width", "185px");
+    });
 });
