1b82468a708bc368f7cb7c6d21e5cc29cac293fc
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-07-11 13:45:33 +0200

    updating flight-form validation

--- assets/js/flight-service.js
+++ assets/js/flight-service.js
@@ -1,16 +1,77 @@
 var FlightService = {
 	init: function () {
-		$("#options-booking-form").on("submit", function (event) {
-			event.preventDefault();
-			var searchData = {
-				departure_airport: $("#from-input").val(),
-				departure_date: $("#departure-date").val(),
-				destination_airport: $("#to-input").val(),
-				arrival_date: $("#return-date").val(),
-			};
-			FlightService.searchFlights(searchData);
-		});
+		// Add method for date validation
+        $.validator.addMethod("date", function(value, element) {
+            return this.optional(element) || !/Invalid|NaN/.test(new Date(value).toString());
+        }, "Please enter a valid date.");
+
+        // Validation for search flight form
+        $("#flight-search-form").validate({
+            rules: {
+                "from-input": {
+                    required: true,
+                },
+                "to-input": {
+                    required: true,
+                },
+                "departure-date": {
+                    required: true,
+                    date: true
+                },
+                "return-date": {
+                    required: true,
+                    date: true
+                },
+                passengers: {
+                    required: true,
+                },
+                "ticket-class": {
+                    required: true,
+                }
+            },
+            messages: {
+                "from-input": {
+                    required: "From field is required",
+                },
+                "to-input": {
+                    required: "To field is required",
+                },
+                "departure-date": {
+                    required: "Departure date is required",
+                },
+                "return-date": {
+                    required: "Return date is required",
+                },
+                passengers: {
+                    required: "Passenger field is required",
+                },
+                "ticket-class": {
+                    required: "Ticket class field is required",
+                }
+            },
+
+			submitHandler: function(form, event) {
+                event.preventDefault();
+                var searchData = {
+                    departure_airport: $("#from-input").val(),
+                    departure_date: $("#departure-date").val(),
+                    destination_airport: $("#to-input").val(),
+                    arrival_date: $("#return-date").val(),
+                };
+                FlightService.searchFlights(searchData);
+            },
+			errorPlacement: function(error, element) {
+                FlightService.displayError(error.text(), element.attr("id"));
+            }
+        });
 	},
+	displayError: function(message, targetId) {
+        var errorHtml = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>`;
+        $(`#${targetId}`).html(errorHtml);
+    },
 
 	searchFlights: function (searchData) {
 		$.ajax({
@@ -30,3 +91,6 @@ var FlightService = {
 		});
 	},
 };
+$(document).ready(function() {
+    FlightService.init();
+});
